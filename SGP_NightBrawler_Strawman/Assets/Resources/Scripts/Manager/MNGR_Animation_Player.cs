04a746068f40bc26d5deebfd6612ab8ca62b90d9
Logan <lmstallman@fullsail.edu>
2015-05-16 16:18:07 -0400

    Getting ready for shiny new things.

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Manager/MNGR_Animation_Player.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Manager/MNGR_Animation_Player.cs
@@ -7,7 +7,8 @@ public class MNGR_Animation_Player : MonoBehaviour {
     Sprite[] sprites;
     ACT_CHAR_Base currentCharacter;
     ACT_CHAR_Base.STATES plyState;
-    float maxTmr;
+    float[] maxTmr;
+    int tmrIndex;
     float curTmr;
     bool loop;
     int[] idleSprites;
@@ -30,8 +31,9 @@ public class MNGR_Animation_Player : MonoBehaviour {
         //plyState = ACT_CHAR_Base.STATES.IDLE;
         sprites = Resources.LoadAll<Sprite>(filepaths[currentCharacter.characterIndex]);
 
-        maxTmr = 2.0f;
-        curTmr = maxTmr;
+        maxTmr = new float[] { 2.0f, 0.75f, 0.5f, 0.2f, 0.2f, 0.2f, 1.0f, 1.0f, 1.0f };
+        tmrIndex = 0;
+        curTmr = maxTmr[tmrIndex];
         loop = true;
 
         // Works for swordsman...
@@ -49,8 +51,8 @@ public class MNGR_Animation_Player : MonoBehaviour {
 
         if (currentCharacter != GetComponent<PlayerController>().party[GetComponent<PlayerController>().currChar])
             currentCharacter = GetComponent<PlayerController>().party[GetComponent<PlayerController>().currChar];
-        //if (plyState != currentCharacter.state)
-            //ChangeState(currentCharacter.state);
+        if (plyState != currentCharacter.state)
+            ChangeState(currentCharacter.state);
 
         if (curTmr > 0)
         {
@@ -58,7 +60,7 @@ public class MNGR_Animation_Player : MonoBehaviour {
             if (curTmr < 0)
             {
                 //EndOfAnim(); // Engage things to do when the animation loops/ ends.
-                curTmr = loop? maxTmr: 0; // reset to maxTmr if looping, otherwise set to 0 and stop updating timer.
+                curTmr = loop ? maxTmr[tmrIndex] : 0; // reset to maxTmr if looping, otherwise set to 0 and stop updating timer.
             }
         }
 
@@ -70,78 +72,95 @@ public class MNGR_Animation_Player : MonoBehaviour {
         switch(plyState)
         {
             case ACT_CHAR_Base.STATES.IDLE:
-                if (curTmr > maxTmr * 0.6f)
+
+                tmrIndex = (int)ACT_CHAR_Base.STATES.IDLE;
+                if (curTmr > maxTmr[tmrIndex] * 0.6f)
                     GetComponent<SpriteRenderer>().sprite = sprites[idleSprites[0]];
-                else if (curTmr > maxTmr * 0.5f)
+                else if (curTmr > maxTmr[tmrIndex] * 0.5f)
                     GetComponent<SpriteRenderer>().sprite = sprites[idleSprites[1]];
-                else if (curTmr > maxTmr * 0.1f)
+                else if (curTmr > maxTmr[tmrIndex] * 0.1f)
                     GetComponent<SpriteRenderer>().sprite = sprites[idleSprites[2]];
                 else if (curTmr >= 0)
                     GetComponent<SpriteRenderer>().sprite = sprites[idleSprites[1]];
                 break;
             case ACT_CHAR_Base.STATES.WALKING:
 
-                if (curTmr > maxTmr * 0.8f)
+                tmrIndex = (int)ACT_CHAR_Base.STATES.WALKING;
+                if (curTmr > maxTmr[tmrIndex] * 0.8f)
                     GetComponent<SpriteRenderer>().sprite = sprites[walkSprites[0]];
-                else if (curTmr > maxTmr * 0.6f)
+                else if (curTmr > maxTmr[tmrIndex] * 0.6f)
                     GetComponent<SpriteRenderer>().sprite = sprites[walkSprites[1]];
-                else if (curTmr > maxTmr * 0.4f)
+                else if (curTmr > maxTmr[tmrIndex] * 0.4f)
                     GetComponent<SpriteRenderer>().sprite = sprites[walkSprites[2]];
-                else if (curTmr > maxTmr * 0.2f)
+                else if (curTmr > maxTmr[tmrIndex] * 0.2f)
                     GetComponent<SpriteRenderer>().sprite = sprites[walkSprites[3]];
                 else if (curTmr >= 0)
                     GetComponent<SpriteRenderer>().sprite = sprites[walkSprites[4]];
                 break;
             case ACT_CHAR_Base.STATES.DASHING:
+
+                tmrIndex = (int)ACT_CHAR_Base.STATES.DASHING;
                 if (currentCharacter.Act_facingRight)
                     transform.localEulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
                 else
                     transform.localEulerAngles = new Vector3(0.0f, 180.0f, 0.0f);
 
-                if (curTmr > maxTmr * 0.8f)
+                if (curTmr > maxTmr[tmrIndex] * 0.8f)
                     GetComponent<SpriteRenderer>().sprite = sprites[walkSprites[0]];
-                else if (curTmr > maxTmr * 0.6f)
+                else if (curTmr > maxTmr[tmrIndex] * 0.6f)
                     GetComponent<SpriteRenderer>().sprite = sprites[walkSprites[1]];
-                else if (curTmr > maxTmr * 0.4f)
+                else if (curTmr > maxTmr[tmrIndex] * 0.4f)
                     GetComponent<SpriteRenderer>().sprite = sprites[walkSprites[2]];
-                else if (curTmr > maxTmr * 0.2f)
+                else if (curTmr > maxTmr[tmrIndex] * 0.2f)
                     GetComponent<SpriteRenderer>().sprite = sprites[walkSprites[3]];
                 else if (curTmr >= 0)
                     GetComponent<SpriteRenderer>().sprite = sprites[walkSprites[4]];
                 break;
             case ACT_CHAR_Base.STATES.ATTACK_1:
-                if (curTmr > maxTmr * 0.75f)
+
+                tmrIndex = (int)ACT_CHAR_Base.STATES.ATTACK_1;
+                if (curTmr > maxTmr[tmrIndex] * 0.75f)
                     GetComponent<SpriteRenderer>().sprite = sprites[attack1Sprites[0]];
-                else if (curTmr > maxTmr * 0.5f)
+                else if (curTmr > maxTmr[tmrIndex] * 0.5f)
                     GetComponent<SpriteRenderer>().sprite = sprites[attack1Sprites[1]];
-                else if (curTmr > maxTmr * 0.25f)
+                else if (curTmr > maxTmr[tmrIndex] * 0.25f)
                     GetComponent<SpriteRenderer>().sprite = sprites[attack1Sprites[2]];
                 else if (curTmr >= 0)
                     GetComponent<SpriteRenderer>().sprite = sprites[attack1Sprites[3]];
                 break;
             case ACT_CHAR_Base.STATES.ATTACK_2:
-                if (curTmr > maxTmr * 0.66f)
+
+                tmrIndex = (int)ACT_CHAR_Base.STATES.ATTACK_2;
+                if (curTmr > maxTmr[tmrIndex] * 0.66f)
                     GetComponent<SpriteRenderer>().sprite = sprites[attack2Sprites[0]];
-                else if (curTmr > maxTmr * 0.66f)
+                else if (curTmr > maxTmr[tmrIndex] * 0.66f)
                     GetComponent<SpriteRenderer>().sprite = sprites[attack2Sprites[1]];
                 else if (curTmr >= 0)
                     GetComponent<SpriteRenderer>().sprite = sprites[attack2Sprites[2]];
                 break;
             case ACT_CHAR_Base.STATES.ATTACK_3:
-                if (curTmr > maxTmr * 0.75f)
+
+                tmrIndex = (int)ACT_CHAR_Base.STATES.ATTACK_3;
+                if (curTmr > maxTmr[tmrIndex] * 0.75f)
                     GetComponent<SpriteRenderer>().sprite = sprites[attack3Sprites[0]];
-                else if (curTmr > maxTmr * 0.5f)
+                else if (curTmr > maxTmr[tmrIndex] * 0.5f)
                     GetComponent<SpriteRenderer>().sprite = sprites[attack3Sprites[1]];
-                else if (curTmr > maxTmr * 0.25f)
+                else if (curTmr > maxTmr[tmrIndex] * 0.25f)
                     GetComponent<SpriteRenderer>().sprite = sprites[attack3Sprites[2]];
                 else if (curTmr >= 0)
                     GetComponent<SpriteRenderer>().sprite = sprites[attack3Sprites[3]];
                 break;
             case ACT_CHAR_Base.STATES.SPECIAL:
+
+                tmrIndex = (int)ACT_CHAR_Base.STATES.SPECIAL;
                 break;
             case ACT_CHAR_Base.STATES.HURT:
+
+                tmrIndex = (int)ACT_CHAR_Base.STATES.HURT;
                 break;
             case ACT_CHAR_Base.STATES.DYING:
+
+                tmrIndex = (int)ACT_CHAR_Base.STATES.DYING;
                 break;
             case ACT_CHAR_Base.STATES.USE:
                 // Special FX because I didn't actually animate these
