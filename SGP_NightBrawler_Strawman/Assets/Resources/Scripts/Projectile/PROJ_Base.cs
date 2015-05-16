477c8e5724b561a6ba6c6c14dfe5ef0e02821323
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2015-05-16 17:21:45 -0400

    Sam: added some comments to the projectile code

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Projectile/PROJ_Base.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Projectile/PROJ_Base.cs
@@ -1,37 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PROJ_Base : MonoBehaviour 
+public class PROJ_Base : MonoBehaviour
 {
+    public GameObject owner;  // who fired us?
+    Vector2 velocity;         // which way are we going?
+    int power;                // how much damage will we deal?
+    Rigidbody2D myBody;       // PHYSICS
+    //bool isPiercing;
 
-    Vector2 velocity;
-    int power;
-    Rigidbody2D myBody;
-    bool isPiercing;
+    public float speed;       // how fast are we moving?
 
-    public float speed;
-
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start()
     {
         myBody = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () 
-    {
-	}
 
-    virtual void OnCollisonEnter2D(Collider2D other)
-    {
-        if(other.gameObject.tag == "Player")
+        // Are you my mommy?
+        if(owner.tag == "Player")
         {
-            int target = other.GetComponent<PlayerController>().currChar;
-            other.GetComponent<PlayerController>().party[target].ChangeHP(-power);
+            bool right = owner.GetComponent<PlayerController>().party[owner.GetComponent<PlayerController>().currChar].Act_facingRight;
+
+            if (right)
+                velocity = new Vector2(1, 0) * speed;
+            else
+                velocity = new Vector2(-1, 0) * speed;
         }
-        else if (other.gameObject.tag == "Enemy")
+        else if (owner.tag == "Enemy")
         {
-            other.GetComponent<ACT_Enemy>().ChangeHP(-power);
+            bool right = owner.GetComponent<ACT_Enemy>().Act_facingRight;
+
+            if (right)
+                velocity = new Vector2(1, 0) * speed;
+            else
+                velocity = new Vector2(-1, 0) * speed;
+        }
+    }
+
+    public virtual void OnCollisonEnter2D(Collider2D other)
+    {
+        // We didn't hit our owner did we?
+        if (other.gameObject != owner)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                int target = other.GetComponent<PlayerController>().currChar;
+                other.GetComponent<PlayerController>().party[target].ChangeHP(-power);
+                Destroy(gameObject); // I'll just show myself out
+            }
+            else if (other.gameObject.tag == "Enemy")
+            {
+                other.GetComponent<ACT_Enemy>().ChangeHP(-power);
+                Destroy(gameObject); // I'll just show myself out
+            }
         }
     }
 }
