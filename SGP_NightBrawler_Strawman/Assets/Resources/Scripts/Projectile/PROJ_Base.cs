2235b93acfbf01260f6d9e5170fdf1a083f3eabc
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2015-05-16 15:53:00 -0400

    Sam: Started work on projectile class

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Projectile/PROJ_Base.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Projectile/PROJ_Base.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class PROJ_Base : MonoBehaviour {
 
+    Vector2 velocity;
+    int power;
+    Rigidbody2D myBody;
+
+    public float speed;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
 	}
+
+    void OnCollisonEnter2D(Collider2D other)
+    {
+
+    }
 }
