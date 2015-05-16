87c12776bd242cb07e431e4531a77f6d5a454a0b
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2015-05-16 17:00:00 -0400

    Sam: miniscule work on projectile base

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Projectile/PROJ_Base.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Projectile/PROJ_Base.cs
@@ -1,18 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PROJ_Base : MonoBehaviour {
+public class PROJ_Base : MonoBehaviour 
+{
 
     Vector2 velocity;
     int power;
     Rigidbody2D myBody;
+    bool isPiercing;
 
     public float speed;
 
 	// Use this for initialization
 	void Start () 
     {
-
+        myBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -22,6 +24,14 @@ public class PROJ_Base : MonoBehaviour {
 
     void OnCollisonEnter2D(Collider2D other)
     {
-
+        if(other.gameObject.tag == "Player")
+        {
+            int target = other.GetComponent<PlayerController>().currChar;
+            other.GetComponent<PlayerController>().party[target].ChangeHP(-power);
+        }
+        else if (other.gameObject.tag == "Enemy")
+        {
+            other.GetComponent<ACT_Enemy>().ChangeHP(-power);
+        }
     }
 }
