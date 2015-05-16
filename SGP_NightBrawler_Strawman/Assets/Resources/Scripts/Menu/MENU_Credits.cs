30eeedb498bd8b8ca8b8266ca48712b7c665e9ba
Javier Tellez <JavierTAcevedo@gmail.com>
2015-05-16 15:53:46 -0400

    Finish Main menu functionality

---
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Menu/MENU_Credits.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MENU_Credits : MonoBehaviour {
+
+    public AudioClip Menu_SelectedSound;
+
+    public void Return()
+    {
+        AudioSource.PlayClipAtPoint(Menu_SelectedSound, new Vector3(0, 0, 0), 1.0f);
+        StartCoroutine(WaitForSound());
+    }
+
+
+
+    IEnumerator WaitForSound()
+    {
+        yield return new WaitForSeconds(0.35f);
+        Application.LoadLevel("MainMenu");
+    }
+}
