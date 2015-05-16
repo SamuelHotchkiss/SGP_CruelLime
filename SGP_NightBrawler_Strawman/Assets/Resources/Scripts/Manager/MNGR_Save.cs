8e75f7c06d2f54c6a297b1cb911440c011ecf0e1
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2015-05-15 20:14:05 -0400

    Sam: Dummy LoadGame Menu
    Created a placeholder load scene with buttons, these buttons don't
    currently do much.

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Manager/MNGR_Save.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Manager/MNGR_Save.cs
@@ -9,16 +9,27 @@ public static class MNGR_Save
 {
     public static List<MNGR_GameData> saveFiles = new List<MNGR_GameData>();
 
+    public static int currSave;
+
+    // Sets up the three save files
+    public static void InitializeProfiles()
+    {
+        for (int i = 0; i < 3; i++)
+            saveFiles.Add(new MNGR_GameData());
+
+        Save();
+    }
+
+    // Saves out the save files to an exterior file
     public static void Save()
     {
-        saveFiles.Add(new MNGR_GameData());
-
         BinaryFormatter bff = new BinaryFormatter();
         FileStream file = File.Create("Assets/Resources/savedGames.SAMMICH");
         bff.Serialize(file, saveFiles);
         file.Close();
     }
 
+    // Loads in the save files from an exterior file
     public static void Load()
     {
         if (File.Exists("Assets/Resources/savedGames.SAMMICH"))
@@ -28,6 +39,20 @@ public static class MNGR_Save
             saveFiles = (List<MNGR_GameData>)bff.Deserialize(file);
             file.Close();
         }
+        else
+            InitializeProfiles();
+    }
+
+    // Only overwrites the data of one save profile
+    public static void OverwriteCurrentSave()
+    {
+        saveFiles[currSave].CopyGameManager();
+    }
+
+    // Only assigns the GameManager from currently loaded save
+    public static void LoadCurrentSave()
+    {
+        saveFiles[currSave].AssignGameManager();
     }
 }
 
