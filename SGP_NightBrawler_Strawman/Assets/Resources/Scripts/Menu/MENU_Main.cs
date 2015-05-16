dae3cc016e7e6909af3b590607dc5a382b19a129
Javier Tellez <JavierTAcevedo@gmail.com>
2015-05-16 12:38:45 -0400

    Fix Controller input

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Menu/MENU_Main.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Menu/MENU_Main.cs
@@ -8,13 +8,20 @@ public class MENU_Main : MonoBehaviour {
 
     public Button[] Menu_UIButtons;
     public int Menu_CurrButton;
+    public float Menu_JoyController;
+
+    private float Menu_JoyTimer;
+
 	// Use this for initialization
 	void Start () {
         Menu_CurrButton = 0;
+        Menu_JoyTimer = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        Menu_JoyController = Input.GetAxisRaw("Pad_Vertical");
 
         for (int i = 0; i < Menu_UIButtons.Length; i++)
         {
@@ -25,7 +32,7 @@ public class MENU_Main : MonoBehaviour {
             }
         }
 
-
+        
 
         if (Input.anyKeyDown)
         {
@@ -41,22 +48,35 @@ public class MENU_Main : MonoBehaviour {
                 if (Menu_CurrButton >= Menu_UIButtons.Length)
                     Menu_CurrButton = Menu_UIButtons.Length - 1;
             }
+
+            if (Input.GetKey(KeyCode.LeftAlt))
+                if (Input.GetKeyDown(KeyCode.F4))
+                {
+                    Debug.Log("Quit");
+                    //Application.Quit();
+                }
         }
 
-        
-        if (Input.GetAxisRaw("Pad_Vertical") > 0.0f)
+
+        if (Menu_JoyTimer <= 0.0f)
         {
-            Menu_CurrButton--;
-            if (Menu_CurrButton < 0)
-                Menu_CurrButton = 0;
-        }
-        else if (Input.GetAxisRaw("Pad_Vertical") < 0.0f)
-        {
-            Menu_CurrButton++;
-            if (Menu_CurrButton >= Menu_UIButtons.Length)
-                Menu_CurrButton = Menu_UIButtons.Length - 1;
+            if (Input.GetAxis("Pad_Vertical") > 0.07f)
+            {
+                Menu_CurrButton--;
+                if (Menu_CurrButton < 0)
+                    Menu_CurrButton = 0;
+            }
+            else if (Input.GetAxis("Pad_Vertical") < -0.07f)
+            {
+                Menu_CurrButton++;
+                if (Menu_CurrButton >= Menu_UIButtons.Length)
+                    Menu_CurrButton = Menu_UIButtons.Length - 1;
+            }
+
+            Menu_JoyTimer = 0.5f;
         }
 
+        Menu_JoyTimer -= Time.deltaTime;
         Menu_UIButtons[Menu_CurrButton].Select();
 	}
 }
