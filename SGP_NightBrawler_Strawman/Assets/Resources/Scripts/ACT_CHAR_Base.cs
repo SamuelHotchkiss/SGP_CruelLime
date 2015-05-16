012dc63457852e1fee58056c9fa76748a29e39e1
Dakota Cardillo <Dcardillo@fullsail.edu>
2015-05-15 20:54:00 -0400

    Player GUI and Base Character Updated
    HUD functionality and base class stuff

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/ACT_CHAR_Base.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/ACT_CHAR_Base.cs
@@ -1,15 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ACT_CHAR_Base : ACT_Base {
+public class ACT_CHAR_Base : ACT_Base 
+{
+	// Remove later
+	public Sprite[] sprites;
+	// 
+
+	public enum STATES { IDLE = 0, WALKING, DASHING, 
+		ATTACK_1, ATTACK_2, ATTACK_3, SPECIAL, HURT, DYING, USE };
+	public STATES state;
+
+	public float cooldownTmrBase;
+	public float cooldownTmr;
+	public int characterIndex;
 
 	// Use this for initialization
-	void Start () {
-	
+	public void Start () 
+	{
+		cooldownTmrBase = 3;
+		cooldownTmr = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	public void Update () 
+	{
+		cooldownTmr -= Time.deltaTime;
+		if (cooldownTmr < 0)
+		{
+			cooldownTmr = 0;
+		}
+	}
+
+	void Render()
+	{
+		switch (state)
+		{
+		case STATES.IDLE:
+				break;
+		case STATES.WALKING:
+				break;
+		case STATES.DASHING:
+				break;
+		case STATES.ATTACK_1:
+				break;
+		case STATES.ATTACK_2:
+				break;
+		case STATES.ATTACK_3:
+				break;
+		case STATES.SPECIAL:
+				break;
+		case STATES.HURT:
+				break;
+		case STATES.DYING:
+				break;
+		case STATES.USE:
+				break;
+		}
+	}
+
+	public virtual void ActivateSpecial()
+	{
+
 	}
 }
--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/UI_HUD.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/UI_HUD.cs
@@ -1,26 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class UI_HUD : MonoBehaviour {
 
 	public Canvas theCanvas;
-	public GameObject Party;
-	public GameObject character_GUI_1, 
-					character_GUI_2,
-					character_GUI_3;
+	public GameObject party;
+
+	public Image fighterHealth;
+	public Image fighterCooldown;
+
+	public Image rangerHealth;
+	public Image rangerCooldown;
+
+	public Image mageHealth;
+	public Image mageCooldown;
+
+	ACT_CHAR_Base fighter, ranger, mage;
 
 	// Use this for initialization
-	void Start () 
+	public void Initialize () 
 	{
 		Cursor.visible = false;
+
+		fighter = party.GetComponent<PlayerController>().party[0];
+		ranger = party.GetComponent<PlayerController>().party[1];
+		mage = party.GetComponent<PlayerController>().party[2];
+
+		fighter.Start();
+		ranger.Start();
+		mage.Start();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+	
+		fighterHealth.fillAmount = (float)(fighter.Act_currHP / fighter.Act_baseHP);
+		fighterCooldown.fillAmount = (float)((fighter.cooldownTmrBase - fighter.cooldownTmr) / fighter.cooldownTmrBase);
+
+		rangerHealth.fillAmount = (float)(ranger.Act_currHP / ranger.Act_baseHP);
+		rangerCooldown.fillAmount = (float)((ranger.cooldownTmrBase - ranger.cooldownTmr) / ranger.cooldownTmrBase);
+
+		mageHealth.fillAmount = (float)(mage.Act_currHP / mage.Act_baseHP);
+		mageCooldown.fillAmount = (float)((mage.cooldownTmrBase - mage.cooldownTmr) / mage.cooldownTmrBase);
+
+
+		fighter.Update();
+		ranger.Update();
+		mage.Update();
+
+
+
 		if (Input.GetKeyDown(KeyCode.Escape))
 		{
-			Party.SetActive(false);
+			party.SetActive(false);
 			theCanvas.transform.GetChild(3).gameObject.SetActive(true);
 			Cursor.visible = true;
 		}
@@ -28,7 +61,7 @@ public class UI_HUD : MonoBehaviour {
 
 	public void ResumeGame()
 	{
-		Party.SetActive(true);
+		party.SetActive(true);
 		theCanvas.transform.GetChild(3).gameObject.SetActive(false);
 		Cursor.visible = false;
 	}
