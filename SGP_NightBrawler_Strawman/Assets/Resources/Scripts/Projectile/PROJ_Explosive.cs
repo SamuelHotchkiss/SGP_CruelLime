8b847cf58b2166512ac3b923590be1ce8c769346
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2015-05-18 19:42:33 -0400

    Sam: Explosive projectile started

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Projectile/PROJ_Explosive.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Projectile/PROJ_Explosive.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PROJ_Fireball : PROJ_Base {
+public class PROJ_Fireball : PROJ_Base 
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 	
 	}
 }
