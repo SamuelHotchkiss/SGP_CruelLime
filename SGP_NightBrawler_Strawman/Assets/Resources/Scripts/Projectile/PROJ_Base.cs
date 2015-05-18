fa6d5642b26cee3644edd6633b9f5f0313b30177
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2015-05-18 17:13:00 -0400

    Sam: Projectiles now have a public range value

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Projectile/PROJ_Base.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Projectile/PROJ_Base.cs
@@ -4,10 +4,14 @@ using System.Collections;
 public class PROJ_Base : MonoBehaviour
 {
     public GameObject owner;  // who fired us?
+
+    Vector2 start;            // where are we from?
     Vector2 velocity;         // which way are we going?
     int power;                // how much damage will we deal?
+    public float distance;           // how far have we gone?
 
     public float speed;       // how fast are we moving?
+    public float range;       // how far can we go?
 
     public virtual void Start()
     {
@@ -43,6 +47,14 @@ public class PROJ_Base : MonoBehaviour
         }
 
         GetComponent<Rigidbody2D>().velocity = velocity;
+        start = new Vector2(transform.position.x, transform.position.y);
+    }
+
+    void Update()
+    {
+        distance = Mathf.Sqrt((start.x - transform.position.x) * (start.x - transform.position.x));
+        if (distance >= range)
+            Destroy(gameObject);
     }
 
     public virtual void OnCollisionEnter2D(Collision2D collision)
