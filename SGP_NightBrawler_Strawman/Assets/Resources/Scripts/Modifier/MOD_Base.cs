9c9fdaf10c00fec57e1ced97721976096aa61f87
Javier Tellez <JavierTAcevedo@gmail.com>
2015-05-18 13:14:16 -0400

    Weekend work

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Modifier/MOD_Base.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Modifier/MOD_Base.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MOD_Base : MonoBehaviour {
 
     public bool Mod_IsBuff;
     public float Mod_effectTimer;
+    public MOD_Base[] Mod_effects;
 
     // Use this for initialization
     void Start()
     {
 
     }
+
 	// Update is called once per frame
     public virtual void Update(){
         Mod_effectTimer -= Time.deltaTime;
