cd9a2ce424ea27d36dbba48fb651a1ab4bb347ea
Javier Tellez <JavierTAcevedo@gmail.com>
2015-05-19 14:34:02 -0400

    Added Spawn Behavior functionality

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Behavior/BHR_Spawner.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Behavior/BHR_Spawner.cs
@@ -3,43 +3,56 @@ using System.Collections;
 
 public class BHR_Spawner : BHR_Base
 {
-    public Vector3 Spw_SpawnPosition;
-    public GameObject[] Spw_CrittersTypes;
+    public bool Spw_SpawnAllCritters;
+    public int Spw_CritterThreshold;
+    public int Spw_CrittersCreated;
     public float Spw_SpawnCoolDown;
     public float Spw_SpawnPerSec;
-    public int Spw_SpawnCritterNum;
-    public bool Spw_SpawnAllCritters;
+
+    public Vector3 Spw_SpawnPosition;
+    public Vector2 Spw_Force; 
+    public GameObject Spw_Critter;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        Spw_SpawnAllCritters = true;
+        Spw_SpawnCoolDown = 20.0f;
+        Spw_SpawnPerSec = 1.0f;
+        Spw_SpawnPosition = owner.transform.position;
+        Spw_CrittersCreated = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
+	void Update () 
+    {
         Spw_SpawnCoolDown -= Time.deltaTime;
         Spw_SpawnPerSec -= Time.deltaTime;
-        
-	}
-	public override void PerformBehavior()
-	{
-        Vector3 TargetPos = owner.target.transform.position;
-        Vector3 HostPos = owner.transform.position;
 
-        //distThresh Is suppose to be use here but is not use in ACT_Enemy
-
-        float Dist = Vector3.Distance(TargetPos, HostPos);
-        if (Spw_SpawnCoolDown > 0.0f)
+        if (Spw_SpawnCoolDown <= 0 )
         {
-            if (Dist <= 10.0f)
-            {
-                Instantiate(Spw_CrittersTypes[0], Spw_SpawnPosition, new Quaternion());
-            } 
+            Spw_SpawnAllCritters = !Spw_SpawnAllCritters;
+            Spw_SpawnCoolDown = 20.0f;
         }
 
-		Debug.Log("Buffer Activated!");
+        if (Spw_CritterThreshold == Spw_CrittersCreated)
+            Spw_SpawnAllCritters = false;
 	}
 
+	public override void PerformBehavior()
+	{
+        if (Spw_SpawnAllCritters)
+        {
+            if (Spw_SpawnPerSec <= 0.0f)
+            {
+                Vector3 ActSpawn = new Vector3(Random.Range(Spw_SpawnPosition.x - 1, Spw_SpawnPosition.x + 1), Random.Range(Spw_SpawnPosition.y - 1, Spw_SpawnPosition.y + 1));
 
+                Instantiate(Spw_Critter, ActSpawn, new Quaternion());
+                Spw_Critter.GetComponent<Rigidbody2D>().AddForce(Spw_Force);
+                Spw_SpawnPerSec = 1.0f;
+                Spw_CrittersCreated++;
+            } 
+        }
+		Debug.Log("Spawner Activated!");
+	}
 }
