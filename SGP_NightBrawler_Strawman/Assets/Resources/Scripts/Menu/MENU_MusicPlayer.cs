88a34b231f507aa5b68f5ffacddd56a360b52cbd
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2015-05-19 19:09:37 -0400

    Sam: Created a background music player for the menus

---
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Menu/MENU_MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MENU_MusicPlayer : MonoBehaviour 
+{
+    static MENU_MusicPlayer mySelf;
+
+	void Awake()
+    {
+        if (mySelf == null)
+            mySelf = this;
+        else
+            Destroy(gameObject);
+
+        GetComponent<AudioSource>().volume = MNGR_Options.musicVol;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        GetComponent<AudioSource>().volume = MNGR_Options.musicVol;
+    }
+}
