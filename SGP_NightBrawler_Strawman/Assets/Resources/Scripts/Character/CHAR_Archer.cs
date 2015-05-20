388aec7aec0946680311e1378d1d8bfb9501b98b
Logan <lmstallman@fullsail.edu>
2015-05-20 18:23:35 -0400

    Swordsman functionally functional.

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Character/CHAR_Archer.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Character/CHAR_Archer.cs
@@ -11,13 +11,15 @@ public class CHAR_Archer : ACT_CHAR_Base
 		cooldownTmr = 0;
 
 		Act_baseHP = 100;
-		Act_basePower = 100;
-		Act_baseSpeed = 15;
-		Act_currHP = 100;
-		Act_currPower = 100;
-		Act_currSpeed = 100;
+        Act_basePower = 100;
+        Act_baseSpeed = 15;
+        //Act_baseSpeed = 32;
+        Act_baseAspeed = 0.02f;
 
-		//sprites = Resources.LoadAll<Sprite>("Sprites/Player/Archer");
+
+        // Remove this comment when the below set of timers has been modified to be different from the Swordsman's
+        //-----Labels4dayz-----   IDLE, WALK, DODGE, ATT1, ATT2, ATT3, SPEC, HURT, DED,  USE
+        StateTmrs = new float[] { 2.0f, 0.75f, 0.1f, 0.6f, 0.5f, 0.8f, 1.0f, 0.1f, 1.0f, 1.0f };
 	}
 
 	// Use this for initialization
--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Character/CHAR_Wizard.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Character/CHAR_Wizard.cs
@@ -11,12 +11,14 @@ public class CHAR_Wizard : ACT_CHAR_Base
 
 		Act_baseHP = 100;
 		Act_basePower = 100;
-		Act_baseSpeed = 10;
-		Act_currHP = 100;
-		Act_currPower = 100;
-		Act_currSpeed = 100;
+        Act_baseSpeed = 10;
+        //Act_baseSpeed = 20;
+        Act_baseAspeed = 0.02f;
 
-		//sprites = Resources.LoadAll<Sprite>("Sprites/Player/Mage");
+
+        // Remove this comment when the below set of timers has been modified to be different from the Swordsman's
+        //-----Labels4dayz-----   IDLE, WALK, DODGE, ATT1, ATT2, ATT3, SPEC, HURT, DED,  USE
+        StateTmrs = new float[] { 2.0f, 0.75f, 0.1f, 0.6f, 0.5f, 0.8f, 1.0f, 0.1f, 1.0f, 1.0f };
 	}
 
 	// Use this for initialization
