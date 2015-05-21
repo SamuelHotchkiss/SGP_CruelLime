28e9bed21138fd5ce0fc8b63b0df7dda607de5ff
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2015-05-20 20:17:58 -0400

    Sam: started itemManager

---
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Manager/MNGR_Item.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class MNGR_Item
+{
+
+	public static void AttachModifier(int ID, GameObject victim)
+    {
+        switch(ID)
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+                break;
+    }
+}
