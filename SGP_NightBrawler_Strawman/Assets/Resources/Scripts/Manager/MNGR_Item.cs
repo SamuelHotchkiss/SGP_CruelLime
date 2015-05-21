d7db7369bcdd947a2d1f9dac16cdf926ca7bcecb
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2015-05-20 20:39:45 -0400

    Sam: worked on ItemManager

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Manager/MNGR_Item.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Manager/MNGR_Item.cs
@@ -7,19 +7,27 @@ public static class MNGR_Item
 	public static void AttachModifier(int ID, GameObject victim)
     {
         switch(ID)
+        {
             case 0:
+                victim.AddComponent<MOD_CDDecrease>();
                 break;
             case 1:
+                victim.AddComponent<MOD_DMGIncrease>();
                 break;
             case 2:
+                victim.AddComponent<MOD_DMGProtection>();
                 break;
             case 3:
+                victim.AddComponent<MOD_HPInstant>();
                 break;
             case 4:
+                victim.AddComponent<MOD_HPRegen>();
                 break;
             case 5:
+                victim.AddComponent<MOD_SPDIncrease>();
                 break;
             default:
                 break;
+        }
     }
 }
