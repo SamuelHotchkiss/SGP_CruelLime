388dc0757661c222e9d632e54b646b9bd0062928
Dakota Cardillo <Dcardillo@fullsail.edu>
2015-06-02 15:29:39 -0400

    Debuffer Projectile

---
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Projectile/PROJ_Debuff.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PROJ_Debuff : PROJ_Base {
+
+	public int debuff_ID;
+
+	// Use this for initialization
+	public override void Initialize(bool _r = true)
+	{
+		base.Initialize(_r);
+	}
+	
+	// Update is called once per frame
+	public override void OnTriggerEnter2D(Collider2D collision)
+	{
+		Debug.Log("HIT!");
+		if (collision.gameObject.tag == "Enemy")
+		{
+			ACT_Enemy enemy = collision.gameObject.GetComponent<ACT_Enemy>();
+			enemy.ChangeHP(-power);
+
+			bool existing = false;
+			for (int i = 0; i < enemy.myBuffs.Count; i++)
+			{
+				if (enemy.myBuffs[i].Mod_ModIndexNum == debuff_ID)
+				{
+					enemy.myBuffs[i].EndModifyActor();
+					MNGR_Item.AttachModifier(debuff_ID, collision.gameObject);
+					existing = true;
+				}
+			}
+			if (!existing)
+			{
+				MNGR_Item.AttachModifier(debuff_ID, collision.gameObject);
+			}
+		}
+		else if (collision.gameObject.tag == "Player")
+		{
+			// Find the active character
+			PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+			int target = player.currChar;
+
+			// Mess with the active character
+			player.party[target].ChangeHP(-power);
+
+			// Attach the Debuff
+			bool existing = false;
+			for (int i = 0; i < player.myBuffs.Count ; i++)
+			{
+				if (player.myBuffs[i].Mod_ModIndexNum == debuff_ID)
+				{
+					player.myBuffs[i].EndModifyActor();
+					MNGR_Item.AttachModifier(debuff_ID, collision.gameObject);
+					existing = true;
+				}
+			}
+			if (!existing)
+			{
+				MNGR_Item.AttachModifier(debuff_ID, collision.gameObject);
+			}
+		}
+
+		
+	}
+}
