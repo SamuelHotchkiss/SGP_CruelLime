80d34c70e3b84e78d7f1468319ab80569178986b
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2015-06-03 14:17:45 -0400

    Sam: actually added spellslinger projectiles and flamethrower assets

---
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Projectile/PROJ_SpellSlinger1.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PROJ_SpellSlinger1 : PROJ_Base
+{
+    public GameObject sibling;
+
+    public override void Initialize(bool _r = true)
+    {
+        base.Initialize(_r);
+
+        GameObject clone1, clone2;
+
+        clone1 = (GameObject)Instantiate(sibling, owner.transform.position, Quaternion.identity);
+        clone2 = (GameObject)Instantiate(sibling, owner.transform.position, Quaternion.identity);
+
+        clone1.GetComponent<PROJ_Base>().owner = owner;
+        clone2.GetComponent<PROJ_Base>().owner = owner;
+
+        clone1.GetComponent<PROJ_Base>().Initialize(_r);
+        clone2.GetComponent<PROJ_Base>().Initialize(_r);
+
+        if(_r)
+        {
+            clone1.GetComponent<PROJ_Base>().velocity = new Vector2(1, 0.25f);
+            clone2.GetComponent<PROJ_Base>().velocity = new Vector2(1, -0.25f);
+        }
+        else
+        {
+            clone1.GetComponent<PROJ_Base>().velocity = new Vector2(-1, 0.25f);
+            clone2.GetComponent<PROJ_Base>().velocity = new Vector2(-1, -0.25f);
+        }
+    }
+
+}
