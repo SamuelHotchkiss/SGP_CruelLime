3a432f7d951c54e959bd763b277d9c1d0989a395
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2015-06-04 15:06:51 -0400

    Sam: Saving and loading now properly work, now saves to the ABYSS

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Menu/MENU_LoadText.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Menu/MENU_LoadText.cs
@@ -7,12 +7,22 @@ public class MENU_LoadText : MonoBehaviour
 {
     public int profileNum;
 
+    public Text lvlName, cashAmount;
+    public Text[] hpAmounts, charLevels;
+
     // Use this for initialization
     public void WriteText()
     {
-        if (!MNGR_Save.saveFiles[profileNum].isNew)
-            GetComponent<Text>().text = MNGR_Save.saveFiles[profileNum].wallet.ToString();
-        else
-            GetComponent<Text>().text = "START NEW GAME";
+        lvlName.text = MNGR_Save.saveFiles[profileNum].currentLevel;
+
+        cashAmount.text = "C o i n s : " + MNGR_Save.saveFiles[profileNum].wallet.ToString();
+
+        for (int i = 0; i < 3; i++)
+        {
+            hpAmounts[i].text = "H P : " + MNGR_Save.saveFiles[profileNum].currentParty[i].Act_currHP.ToString();
+
+            MNGR_Save.saveFiles[profileNum].currentParty[i].Act_AverageLevel = MNGR_Save.saveFiles[profileNum].currentParty[i].CalcAverageLvl();
+            charLevels[i].text = "L e v e l : " + MNGR_Save.saveFiles[profileNum].currentParty[i].Act_AverageLevel.ToString();
+        }
     }
 }
