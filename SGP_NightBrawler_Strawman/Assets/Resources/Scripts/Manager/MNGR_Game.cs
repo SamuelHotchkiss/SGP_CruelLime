36c01b6627d3e9fb649a251461de13b3c2620d6c
Sam Hotchkiss <wolfgang.hotchkiss@gmail.com>
2015-06-10 18:40:45 -0400

    Sam: lighting partially implemented

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Manager/MNGR_Game.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Manager/MNGR_Game.cs
@@ -17,7 +17,7 @@ public static class MNGR_Game
 	public static int equippedItem;
 
 	public static MNGR_Inventory theInventory = new MNGR_Inventory();
-	public static bool isNight;
+	public static bool isNight = false;
 	public static int hordePosition, HordeDelay, playerPosition, wallet;
 	public static bool paused;
 	public static bool dangerZone;      // don't they know you live in the DANGAH ZOWN?! // determines if the horde is on the same level as the player
@@ -31,6 +31,10 @@ public static class MNGR_Game
         theMan = true;
 
         currentLevel = "NEW GAME";
+
+        //isNight = true;
+        //hordePosition = HordeDelay = playerPosition = wallet = 0;
+        //paused = dangerZone = false;
 
 		theCharacters[0] = new CHAR_Swordsman();
 		theCharacters[1] = new CHAR_Lancer();
