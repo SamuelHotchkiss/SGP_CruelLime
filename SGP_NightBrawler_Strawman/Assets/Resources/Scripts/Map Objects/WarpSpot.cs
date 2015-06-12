cc58db0058433cf3c94e7d4df0414729774bcc6f
Dakota Cardillo <Dcardillo@fullsail.edu>
2015-06-12 17:08:44 -0400

    Forest Level 1 Populated

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Map Objects/WarpSpot.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Map Objects/WarpSpot.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class WarpSpot : MonoBehaviour 
 {
     public GameObject destination;
+	public GameObject enemies;
+	public bool activateEnemies;
 
     // S: hardcoded nonsense to get some basic level functionality
     void OnTriggerEnter2D(Collider2D other)
@@ -13,6 +15,10 @@ public class WarpSpot : MonoBehaviour
             other.transform.position = new Vector3(destination.transform.position.x, destination.transform.position.y, 0);
 			GameObject.Find("Main Camera").gameObject.transform.position = new Vector3(destination.transform.position.x, destination.transform.position.y, -10.0f);
         }
+		if (activateEnemies)
+		{
+			enemies.gameObject.SetActive(true);
+		}
     }
 	
 }
