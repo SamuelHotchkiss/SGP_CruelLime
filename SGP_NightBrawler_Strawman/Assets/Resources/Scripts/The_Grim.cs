0b374e77fbb2c96606e5fa50d5e55e213c806999
Dakota Cardillo <Dcardillo@fullsail.edu>
2015-06-12 21:13:52 -0400

    Changes

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/The_Grim.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/The_Grim.cs
@@ -54,6 +54,7 @@ public class The_Grim : MonoBehaviour
 		AudioSource.PlayClipAtPoint(victory, new Vector3(0, 0, 0), MNGR_Options.musicVol);
 		yield return new WaitForSeconds(5);
 		MNGR_Game.UpdateWorld();
+		MNGR_Game.NextLevel = "WorldMap";
 		Application.LoadLevel("TransitionScene");
 	}
 }
