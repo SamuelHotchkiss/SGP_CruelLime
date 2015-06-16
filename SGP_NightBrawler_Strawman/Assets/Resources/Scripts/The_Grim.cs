e11685dedc50f5e4425fc342270695cf4982a047
Dakota Cardillo <Dcardillo@fullsail.edu>
2015-06-16 13:57:28 -0400

    Boss fights end on schedule

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/The_Grim.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/The_Grim.cs
@@ -3,45 +3,18 @@ using System.Collections;
 
 public class The_Grim : MonoBehaviour 
 {
-	public GameObject boss;
-	private ACT_Enemy script;
 	private AudioClip victory;
 	private bool playMusic = true;
 	// Use this for initialization
 	void Start () 
 	{
 		victory = Resources.Load("Audio/Boss_Win") as AudioClip;
-
-		if (boss.GetComponent<ACT_BOS_Ent>())
-		{
-			script = boss.GetComponent<ACT_BOS_Ent>();
-		}
-		else if (boss.GetComponent<ACT_BOS_Trollgre>())
-		{
-			script = boss.GetComponent<ACT_BOS_Trollgre>();
-		}
-		else if (boss.GetComponent<ACT_BOS_Bipolar>())
-		{
-			script = boss.GetComponent<ACT_BOS_Bipolar>();
-		}
-		else if (boss.GetComponent<ACT_BOS_Miner>())
-		{
-			script = boss.GetComponent<ACT_BOS_Miner>();
-		}
-		else if (boss.GetComponent<ACT_BOS_Dwagon>())
-		{
-			script = boss.GetComponent<ACT_BOS_Dwagon>();
-		}
-		else if (boss.GetComponent<ACT_BOS_Sovalpa>())
-		{
-			script = boss.GetComponent<ACT_BOS_Sovalpa>();
-		}
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (((script.Act_currHP <= 0.0f || script.state == ACT_Enemy.STATES.DEAD) || boss == null) && playMusic)
+		if ((!GameObject.FindGameObjectWithTag("Enemy")) && playMusic)
 		{
 			StartCoroutine("LoadLevel");
 			playMusic = false;
