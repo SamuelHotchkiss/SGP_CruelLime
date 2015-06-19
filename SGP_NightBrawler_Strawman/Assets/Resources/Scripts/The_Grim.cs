eb5233ebbf8eaa391edf7ddf6cae1d52489deb00
Dakota Cardillo <Dcardillo@fullsail.edu>
2015-06-19 19:45:29 -0400

    Final boss brings you to game win

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/The_Grim.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/The_Grim.cs
@@ -5,6 +5,7 @@ public class The_Grim : MonoBehaviour
 {
 	private AudioClip victory;
 	private bool playMusic = true;
+	public bool finalLevel;
 	// Use this for initialization
 	void Start () 
 	{
@@ -27,7 +28,10 @@ public class The_Grim : MonoBehaviour
 		AudioSource.PlayClipAtPoint(victory, new Vector3(0, 0, 0), MNGR_Options.musicVol);
 		yield return new WaitForSeconds(5);
 		MNGR_Game.UpdateWorld();
-		MNGR_Game.NextLevel = "WorldMap";
+		if (finalLevel)
+			MNGR_Game.NextLevel = "GameOverWin";
+		else
+			MNGR_Game.NextLevel = "WorldMap";
 		Application.LoadLevel("TransitionScene");
 	}
 }
