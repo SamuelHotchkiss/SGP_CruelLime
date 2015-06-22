e53f6c94b93c2ce523943a7d471ef20b387734da
Javier Tellez <JavierTAcevedo@gmail.com>
2015-06-22 18:52:44 -0400

    FIXING Level forest 1 and 2 so camera events are aline with the floor better

--- SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Modifier/MOD_Base.cs
+++ SGP_NightBrawler_Strawman/Assets/Resources/Scripts/Modifier/MOD_Base.cs
@@ -41,7 +41,6 @@ public class MOD_Base : MonoBehaviour
     // Use this for initialization
     public virtual void Start()
     {
-        Mod_Particles.transform.rotation = new Quaternion(-270, Mod_Particles.transform.rotation.y, Mod_Particles.transform.rotation.z, Mod_Particles.transform.rotation.w);
         Mod_ModIndexNum = -1;           //Base class
         Mod_Particles.gameObject.transform.SetParent(gameObject.transform);
         Mod_Particles.GetComponent<ParticleSystemRenderer>().sortingOrder = 0;
